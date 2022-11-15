ed28e41c122ea8898718dd1233e2d03672a6d609
M Katherine <86696439+kathBD@users.noreply.github.com>
2022-11-15 15:04:19 -0500

    Poo

---
+++ src/main/java/programacion/Depuracion.java
@@ -0,0 +1,33 @@
+package programacion;
+
+public class Depuracion {
+    public static void main(String[] args){
+        funtion1();
+
+      /*  int suma = 0;
+        //suma = suma + 15;
+        suma = suma + 16;
+
+        System.out.println(suma);*/
+
+      /*  int valores[] = new valores[5]:
+        int posicion=5;
+
+        System.out.println(valores[posicion]);*/
+
+    }
+    public static void funtion1(){
+        funtion2();
+    }
+    public static void funtion2(){
+        funtion3();
+    }
+    public static void funtion3(){
+        funtion4();
+    }
+    public static void funtion4(){
+        System.out.println("Vaya viaje que nos estamos dando");
+
+    }
+
+}
---
+++ src/main/java/programacion/PrivacidadAbstracciónEncapsulación.java
@@ -0,0 +1,7 @@
+package programacion;
+
+public class PrivacidadAbstracciónEncapsulación {
+    public static void main(String[] args) {
+
+    }
+}
